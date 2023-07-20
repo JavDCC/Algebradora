12077f68f1c368ee38296a09b89bd928312398be
JavDCC <javierdavidcoroas@gmail.com>
2023-07-20 00:01:00 -0500

    Bash Updated. Almost Fully Functional. :D

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -1,52 +1,72 @@
 #!/bin/bash
-# Javier David Coroas Cintra C113
-# Script del proyecto de practicas laborales MATCOM 2023
 
+#El argumento pasado al script:
 arg=$1
+#La lista de comandos posibles para el script
 list=("run" "clean" "report" "slides" "show_report" "show_slides")
-options=$(echo "${list[*]}" | sed 's/ /,/g')
+
+echo "Javier David Coroas Cintra - C113 - Practicas Laborales MATCOM 2023"
 
 if [ $# -lt 1 ]; then
-    echo -e "Error: El comando debe contener al menos un argumento."
-    echo "Los posibles comandos son: $options."
-    exit 1
+    echo -e "Error: El comando debe contener un argumento."
+    echo "Posibles argumentos: 'run' 'clean' 'report' 'slides' 'show_report' y 'show_slides'."
+else
+    valid=false
+    for item in "${list[@]}"; do
+        if [ "$arg" == "$item" ]; then
+            valid=true
+        fi
+    done
+    if [ $valid == false ]; then
+        echo "Error: $arg no es un argumento valido."
+        echo "Posibles argumentos: 'run' 'clean' 'report' 'slides' 'show_report' y 'show_slides'."
+    else
+        if [ $# -gt 1 ]; then
+            echo "Error: Solo se acepta '$arg' como argumento. Se ignora el resto."
+        else
+            cd ..
+            case $arg in
+            "run")
+                Run
+                ;;
+            "clean")
+                Clean
+                ;;
+            "report")
+                MakePDF "informe"
+                ;;
+            "slides")
+                MakePDF "presentacion"
+                ;;
+            "show_report")
+                #
+                ;;
+            "show_slides")
+                #
+                ;;
+            esac
+        fi
+    fi
 fi
 
-case $arg in
-"run")
-    cd ..
+function Run() {
     dotnet run
-    echo "Ha ejecutado el proyecto."
-    exit 1
-    ;;
-"clean")
-    cd ..
+    echo -e "Se ha ejecutado 'dotnet run'."
+}
+
+function Clean() {
     dotnet clean
     rm -rf bin obj
     echo -e "Se ha ejecutado 'dotnet clean'."
-    echo "Se han eliminado, junto a sus contenidos, los directorios 'bin' y 'obj'."
-    exit 1
-    ;;
-"report")
-    echo "Se ha generado el reporte."
-    exit 1
-    ;;
-"slides")
-    echo "Se ha generado la presentacion."
-    exit 1
-    ;;
-"show_report")
-    echo "$arg"
-    exit 1
-    ;;
-"show_slides")
-    echo "$arg"
-    exit 1
-    ;;
-esac
+    echo "Tambien se han eliminado los directorios 'bin' y 'obj'."
+}
 
-if [ "$arg" != "${list[*]}" ]; then
-    echo -e "Error: $arg no es un argumento valido."
-    echo "Los posibles comandos son: $options."
-    exit 1
-fi
+function MakePDF() {
+    cd "$1" || return 0
+    pdflatex ".tex"
+    echo "Se ha generado el reporte."
+}
+
+function FindPDF() {
+    cd "$1" || return 0
+}
